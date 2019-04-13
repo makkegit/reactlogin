f7c11c2873a5bd8b3558664e32d79aad410633ba
Markus Hakala <markus.hakala@kolumbus.fi>
2019-04-13 13:31:10 +0300

    testrun

--- src/App.js
+++ src/App.js
@@ -3,6 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state ={
+      isLoaded: false,
+      testemail: "",
+      testpassword: "",
+      token: null,
+    }
+  }
+
+  componentDidMount() {
+    const email = "test@testi.fi";
+    const password = "passu";
+  
+    fetch("https://opendata.hopefully.works/api/signup", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        email,
+        password
+      })
+    })
+      .then(res => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw Error(res.statusText);
+        }
+      })
+      .then(json => {
+        this.setState({
+          testemail: email,
+          testpassword: password,
+          isLoaded: true,
+          token: json
+        });
+      })
+      .catch(error => console.error(error));
+  }
   render() {
     return (
       <div className="App">
@@ -10,16 +52,11 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            </p>
         </header>
+        <div>email: {this.state.email} </div>
+        <div>password: {this.state.password} </div>
+        <div>token: {this.state.token} </div>
       </div>
     );
   }
